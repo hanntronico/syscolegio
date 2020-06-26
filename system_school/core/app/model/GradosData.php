4d841bc8a6a820b3d39b3378da1139a8a66b159f
hanntronico <pedro.bernal.84@gmail.com>
2020-06-25 20:16:51 -0500

    eñes en gradosData

--- system_school/core/app/model/GradosData.php
+++ system_school/core/app/model/GradosData.php
@@ -8,6 +8,8 @@ class GradosData {
 	}
 
 	public function add(){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "insert into ".self::$tablename." (nombre,nivel) ";
 		$sql .= "value (\"$this->nombre\",\"$this->nivel\")";
 		Executor::doit($sql);
@@ -24,22 +26,30 @@ class GradosData {
 	}
 
 	public function update(){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "update ".self::$tablename." set nombre=\"$this->nombre\" where id_grado=$this->id";
 		Executor::doit($sql);
 	}
 
 	public function updateById($k,$v){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "update ".self::$tablename." set $k=\"$v\" where id_grado=$this->id";
 		Executor::doit($sql);
 	}
 
 	public static function getById($id){
-		 $sql = "select * from ".self::$tablename." where id_grado=$id";
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
+		$sql = "select * from ".self::$tablename." where id_grado=$id";
 		$query = Executor::doit($sql);
 		return Model::one($query[0],new GradosData());
 	}
 
 	public static function getBy($k,$v){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "select * from ".self::$tablename." where $k=\"$v\"";
 		$query = Executor::doit($sql);
 		return Model::one($query[0],new GradosData());
@@ -54,17 +64,23 @@ class GradosData {
 	}
 
 	public static function getAllBy($k,$v){
-		 $sql = "select * from ".self::$tablename." where $k=\"$v\"";
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
+		$sql = "select * from ".self::$tablename." where $k=\"$v\"";
 		$query = Executor::doit($sql);
 		return Model::many($query[0],new GradosData());
 	}
 	public static function getAllByUserId($id){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "select * from ".self::$tablename." where user_id=$id";
 		$query = Executor::doit($sql);
 		return Model::many($query[0],new GradosData());
 	}
 
 	public static function getLike($q){
+		$sqlsetutf = "set names utf8";
+		$query2 = Executor::doit($sqlsetutf);		
 		$sql = "select * from ".self::$tablename." where name like '%$q%'";
 		$query = Executor::doit($sql);
 		return Model::many($query[0],new GradosData());
